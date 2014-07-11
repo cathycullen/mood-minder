97e6cae625485f737bfd4838ffe1181d4f831c13
Cathy Cullen <cathy@softwareoptions.com>
2014-07-11 10:46:16 -0500

    connected nav components

--- www/js/views/navbar.js
+++ www/js/views/navbar.js
@@ -7,4 +7,32 @@ var NavBarView = function() {
 
     app.editReminderSchedule();
   }.bind(this));
+
+  this.el.find('#mood').click(function(e)	{
+  	e.preventDefault();
+  	this.el.find(".navbar-collapse").collapse('hide');
+
+  	app.postLogin();
+  }.bind(this));
+
+  this.el.find('#login').click(function(e)	{
+  	e.preventDefault();
+  	this.el.find(".navbar-collapse").collapse('hide');
+
+  	app.loginForm();
+  }.bind(this));
+
+   this.el.find('#forgot-password').click(function(e)	{
+  	e.preventDefault();
+  	this.el.find(".navbar-collapse").collapse('hide');
+
+  	app.forgotPassword();
+  }.bind(this));
+
+   this.el.find('#signup').click(function(e)	{
+  	e.preventDefault();
+  	this.el.find(".navbar-collapse").collapse('hide');
+
+  	app.signup();
+  }.bind(this));
 }
